e11bf37ab5ce3a73d4d72e226f3da2153b6b2c86
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-12-01 13:27:33 +0800

    Solved day 1

--- crates/day1/src/main.rs
+++ crates/day1/src/main.rs
@@ -1,19 +1,37 @@
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("input: {:?}", INPUT);
-    // println!("problem1: {}", solve_problem1(INPUT));
-    // println!("problem2: {}", solve_problem2(INPUT));
+    // println!("input: {:?}", INPUT);
+    println!("problem1: {}", solve_problem1(INPUT));
+    println!("problem2: {}", solve_problem2(INPUT));
 }
 
 #[allow(unused)]
-fn solve_problem1(input: &str) -> i32 {
-    todo!()
+fn solve_problem1(input: &str) -> usize {
+    numbers(input)
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count()
 }
 
 #[allow(unused)]
-fn solve_problem2(input: &str) -> i32 {
-    todo!()
+fn solve_problem2(input: &str) -> usize {
+    numbers(input)
+        .windows(3)
+        .map(|window| -> i32 { window.iter().sum() })
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|window| window[0] < window[1])
+        .count()
+}
+
+fn numbers(input: &str) -> Vec<i32> {
+    let numbers = input
+        .trim()
+        .lines()
+        .map(|i| i.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+    numbers
 }
 
 #[cfg(test)]
@@ -22,16 +40,38 @@ mod test {
 
     #[test]
     fn problem1() {
-        let input = "";
-        let expected = 0;
+        let input =  r#"
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"#;
+        let expected = 7;
         let actual = solve_problem1(input);
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn problem2() {
-        let input = "";
-        let expected = 0;
+        let input = r#"
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"#;
+        let expected = 5;
         let actual = solve_problem2(input);
         assert_eq!(expected, actual);
     }
