06dfccc87ebc583e06375517f1502792194ea0f7
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-12-06 13:29:44 +0800

    Solved day6 and cleaned up

--- crates/day6/src/main.rs
+++ crates/day6/src/main.rs
@@ -4,7 +4,6 @@ const INPUT: &str = include_str!("input.txt");
 
 fn main() {
     let input = Input::from_str(INPUT);
-    println!("input: {:?}", input);
 
     println!("problem1: {}", input.problem1());
     println!("problem2: {}", input.problem2());
@@ -12,56 +11,76 @@ fn main() {
 
 #[allow(unused)]
 mod input {
-    #[derive(Debug)]
-    pub struct Line {}
+    use std::collections::HashMap;
 
-    impl Line {
-        pub fn from_str(s: &str) -> Self {
-            Self {}
-        }
-    }
-
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     pub struct Input {
-        inner: Vec<Line>,
+        ages: Vec<usize>,
     }
 
     impl Input {
         pub fn from_str(s: &str) -> Self {
             Self {
-                inner: s.lines().map(Line::from_str).collect(),
+                ages: s
+                    .trim()
+                    .split(',')
+                    .map(|n| n.trim().parse().unwrap())
+                    .collect(),
             }
         }
 
-        pub fn problem1(&self) -> i32 {
-            todo!()
+        pub fn problem1(&self) -> usize {
+            self.population_after_n_days(80)
         }
 
-        pub fn problem2(&self) -> i32 {
-            todo!()
+        pub fn problem2(&self) -> usize {
+            self.population_after_n_days(256)
         }
+
+        fn population_after_n_days(&self, iterations: i32) -> usize {
+            let mut tally = self.tally();
+            for _ in 0..iterations {
+                update_population(&mut tally, 2);
+            }
+            tally.iter().copied().sum()
+        }
+
+        fn tally(&self) -> [usize; 9] {
+            let mut tally = [0; 9];
+            for &age in &self.ages {
+                tally[age] += 1;
+            }
+            tally
+        }
+    }
+
+    fn update_population(tally: &mut [usize], new_spawn_time: usize) {
+        let zero = tally[0];
+        for i in 0..tally.len() - 1 {
+            tally[i] = tally[i + 1];
+        }
+        tally[tally.len() - 1 - new_spawn_time] += zero;
+        tally[tally.len() - 1] = zero;
     }
 }
 #[cfg(test)]
 mod test {
     use crate::Input;
 
+    const INPUT: &str = "3,4,3,1,2";
+
     #[test]
-    #[ignore = "day not available"]
     fn problem1() {
-        let input = "";
-        let input = Input::from_str(input);
-        let expected = 0;
+        let input = Input::from_str(INPUT);
+        let expected = 5934;
         let actual = input.problem1();
         assert_eq!(expected, actual);
     }
 
     #[test]
-    #[ignore = "day not available"]
     fn problem2() {
-        let input = "";
-        let input = Input::from_str(input);
-        let expected = 0;
+        let input = Input::from_str(INPUT);
+        let expected = 26984457539;
         let actual = input.problem2();
         assert_eq!(expected, actual);
     }
