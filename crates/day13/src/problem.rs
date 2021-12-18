7297a4e68ef2440f4ddf404e00b42cb3d17dc4ed
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-12-18 15:53:29 +0800

    Added grid crate

--- crates/day13/src/problem.rs
+++ crates/day13/src/problem.rs
@@ -1,98 +1,48 @@
-use std::{
-    fmt::Display,
-    ops::{Index, IndexMut},
-};
+use grid::Grid;
 
 use crate::input::{Fold, Input, Point};
 
-#[derive(Debug, Clone)]
-pub struct Grid<T> {
-    grid: Vec<T>,
-    width: usize,
-}
-
-impl<T> Index<(usize, usize)> for Grid<T> {
-    type Output = T;
-
-    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
-        let index = x + y * self.width;
-        &self.grid[index]
+fn fold(grid: &Grid<u8>, fold: Fold) -> Grid<u8> {
+    match fold {
+        Fold::X(pos) => fold_x(grid, pos),
+        Fold::Y(pos) => fold_y(grid, pos),
     }
 }
 
-impl<T> IndexMut<(usize, usize)> for Grid<T> {
-    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
-        let index = x + y * self.width;
-        &mut self.grid[index]
-    }
-}
+fn fold_x(grid: &Grid<u8>, pos: usize) -> Grid<u8> {
+    let left = pos;
+    let right = grid.width() - pos - 1;
+    let width = left.max(right);
+    let mut result = Grid::new(b'.', width, grid.height());
 
-impl Display for Grid<u8> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in 0..self.height() {
-            for x in 0..self.width {
-                write!(f, "{}", self[(x, y)] as char)?;
-            }
-            writeln!(f)?;
-        }
-        Ok(())
-    }
-}
-
-impl<T: Clone> Grid<T> {
-    fn new(elem: T, width: usize, height: usize) -> Grid<T> {
-        let grid = vec![elem; width * height];
-        Grid { grid, width }
-    }
-
-    pub fn height(&self) -> usize {
-        self.grid.len() / self.width
-    }
-}
-
-impl Grid<u8> {
-    pub fn fold(&self, fold: Fold) -> Grid<u8> {
-        match fold {
-            Fold::X(pos) => self.fold_x(pos),
-            Fold::Y(pos) => self.fold_y(pos),
-        }
-    }
-
-    pub fn fold_y(&self, pos: usize) -> Grid<u8> {
-        let top = pos;
-        let bottom = self.height() - pos - 1;
-        let height = top.max(bottom);
-        let mut result = Grid::new(b'.', self.width, height);
-
-        for x in 0..self.width {
-            for y in 0..height {
-                let y2 = 2 * height - y;
-                if self[(x, y)] == b'#' || (y2 < self.height() && self[(x, y2)] == b'#') {
-                    result[(x, y)] = b'#';
-                }
+    for y in 0..grid.height() {
+        for x in 0..width {
+            let x2 = 2 * width - x;
+            if grid[(x, y)] == b'#' || (x2 < grid.width() && grid[(x2, y)] == b'#') {
+                result[(x, y)] = b'#';
             }
         }
-
-        result
     }
 
-    pub fn fold_x(&self, pos: usize) -> Grid<u8> {
-        let left = pos;
-        let right = self.width - pos - 1;
-        let width = left.max(right);
-        let mut result = Grid::new(b'.', width, self.height());
+    result
+}
 
-        for y in 0..self.height() {
-            for x in 0..width {
-                let x2 = 2 * width - x;
-                if self[(x, y)] == b'#' || (x2 < self.width && self[(x2, y)] == b'#') {
-                    result[(x, y)] = b'#';
-                }
+fn fold_y(grid: &Grid<u8>, pos: usize) -> Grid<u8> {
+    let top = pos;
+    let bottom = grid.height() - pos - 1;
+    let height = top.max(bottom);
+    let mut result = Grid::new(b'.', grid.width(), height);
+
+    for x in 0..grid.width() {
+        for y in 0..height {
+            let y2 = 2 * height - y;
+            if grid[(x, y)] == b'#' || (y2 < grid.height() && grid[(x, y2)] == b'#') {
+                result[(x, y)] = b'#';
             }
         }
-
-        result
     }
+
+    result
 }
 
 #[derive(Debug, Clone)]
@@ -134,7 +84,7 @@ impl Problem {
             let mut grid = self.grid.clone();
 
             for &fold in &self.folds {
-                grid = grid.fold(fold);
+                grid = crate::problem::fold(&grid, fold);
             }
             println!("{}", grid);
         }
@@ -142,11 +92,11 @@ impl Problem {
     }
 
     pub fn common(&self) -> usize {
-        let grid = self.grid.fold(self.folds[0]);
+        let grid = crate::problem::fold(&self.grid, self.folds[0]);
         count_points(grid)
     }
 }
 
 fn count_points(grid: Grid<u8>) -> usize {
-    grid.grid.into_iter().filter(|&p| p == b'#').count()
+    grid.into_iter().filter(|&p| p == b'#').count()
 }
---
+++ crates/grid/src/lib.rs
@@ -0,0 +1,66 @@
+use std::{ops::{Index, IndexMut}, fmt::Display};
+
+#[derive(Debug, Clone)]
+pub struct Grid<T> {
+    grid: Vec<T>,
+    width: usize,
+}
+
+impl<T> Index<(usize, usize)> for Grid<T> {
+    type Output = T;
+
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        let index = x + y * self.width;
+        &self.grid[index]
+    }
+}
+
+#[test]
+fn it_works() {
+    let result = 2 + 2;
+    assert_eq!(result, 4);
+}
+
+impl<T> IndexMut<(usize, usize)> for Grid<T> {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        let index = x + y * self.width;
+        &mut self.grid[index]
+    }
+}
+
+impl<T: Clone> Grid<T> {
+    pub fn new(elem: T, width: usize, height: usize) -> Grid<T> {
+        let grid = vec![elem; width * height];
+        Grid { grid, width }
+    }
+
+    pub fn height(&self) -> usize {
+        self.grid.len() / self.width
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+}
+
+impl Display for Grid<u8> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.height() {
+            for x in 0..self.width {
+                write!(f, "{}", self[(x, y)] as char)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T> IntoIterator for Grid<T> {
+    type Item = T;
+
+    type IntoIter = std::vec::IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.grid.into_iter()
+    }
+}
