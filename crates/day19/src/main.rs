819302fc98fa602b43b9feeb5b42956ec5f0e892
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-12-23 14:00:07 +0800

    Added template for day 19 from day 18

---
+++ crates/day19/src/main.rs
@@ -0,0 +1,18 @@
+use crate::problem::Problem;
+
+const INPUT: &str = include_str!("input.txt");
+
+fn main() {
+    let input = INPUT.parse().unwrap();
+    let problem = Problem::from_input(input);
+
+    println!("problem1: {}", problem.part1());
+    println!("problem2: {}", problem.part2());
+}
+
+mod input;
+
+mod problem;
+
+#[cfg(test)]
+mod test;
